8183c80139b9b25fc3aab01d1c22f861c8e3fdc8
Vitaly Iegorov <vitalyiegorov@gmail.com>
2015-12-24 10:09:40 +0200

    Refactored Updated Added init end event name as constant

--- Core.php
+++ Core.php
@@ -3,34 +3,41 @@
  * Created by Vitaly Iegorov <egorov@samsonos.com>
  * on 11.04.14 at 15:17
  */
- namespace samson\social;
+namespace samson\social;
 
- use samson\activerecord\dbRecord;
- use samson\core\CompressableService;
- use samsonphp\event\Event;
+use samsonframework\orm\RecordInterface;
+use samson\core\CompressableService;
+use samsonphp\event\Event;
 
  /**
- * Generic class for user authorization
- * @author Vitaly Egorov <egorov@samsonos.com>
- * @copyright 2013 SamsonOS
- * @version 0.1
- */
+  * Generic class for user authorization.
+  * TODO: Avoid $_SESSION using
+  * TODO: Integrate possible javascript client authorization logic
+  * TODO: Refactor code
+  * TODO: Looses db() dependency
+  *
+  * @author Vitaly Egorov <egorov@samsonos.com>
+  * @copyright 2015 SamsonOS
+  */
 class Core extends CompressableService
 {
+    /** Initialization end */
+    const EVENT_INIT_END = 'social.core.init';
+
+    /** @var self[] Collection of social ancestors */
+    protected static $ancestors = array();
+
     /** General hashing algorithm */
     public $hashAlgorithm = 'sha256';
 
     /** General hashing algorithm output size */
     public $hashLength = 64;
 
-    /** @var callable External initialization handler */
-    public $initHandler;
-
     /**
-     * Collection of social ancestors
-     * @var Core[]
+     * @deprecated Should be changed to events logic.
+     * @var callable External initialization handler
      */
-    protected static $ancestors = array();
+    public $initHandler;
 
     /** Module identifier */
     public $id = 'social';
@@ -47,23 +54,21 @@ class Core extends CompressableService
     /** Database user token field */
     public $accessToken = 'accessToken';
 
-    /**
-     * Pointer to current user object
-     * @var dbRecord
-     */
+    /** @var self Pointer to current social module who has authorized */
+    public $active;
+
+    /** @var RecordInterface Pointer to current user object */
     protected $user;
 
     /** Is user authorized */
     protected $authorized = false;
 
-    /** Pointer to current social module who has authorized */
-    public $active;
-
     /**
-     * Update authorization status of all social services
-     * @param dbRecord $user Pointer to authorized user database record
+     * Update authorization status of all social services.
+     *
+     * @param RecordInterface $user Pointer to authorized user database record
      */
-    protected function update(dbRecord & $user)
+    protected function update(RecordInterface &$user)
     {
         // Load user from session
         $this->user = $user;
@@ -74,30 +79,27 @@ class Core extends CompressableService
 
         // Tell all ancestors that we are in
         foreach (self::$ancestors as & $ancestor) {
-            $ancestor->user = & $this->user;
-            $ancestor->active = & $this;
+            $ancestor->user = &$this->user;
+            $ancestor->active = &$this;
             $ancestor->authorized = $this->authorized;
         }
     }
 
-    /**
-     * @return dbRecord Pointer to current authorized user object
-     */
-    public function & user()
+    /** @return RecordInterface Pointer to current authorized user object */
+    public function &user()
     {
         return $this->user;
     }
 
-    /**
-     * @return bool True if user is authorized
-     */
+    /** @return bool True if user is authorized */
     public function authorized()
     {
         return $this->authorized;
     }
 
     /**
-     * Hashing function
+     * Hashing function.
+     *
      * @param string $value Valur for hashing
      * @return string Hashed value
      */
@@ -120,20 +122,21 @@ class Core extends CompressableService
     }
 
     /**
-     * Module initialization
-     * @param array $params
+     * Module initialization.
+     *
+     * @param array $params Initializarion parameters
      */
     public function init(array $params = array())
     {
         // Store this module as ancestor
-        self::$ancestors[$this->id] = & $this;
+        self::$ancestors[$this->id] = &$this;
 
         // If we are not authorized
         if (!$this->authorized) {
+            // TODO: Remove session dependency as storage can be different
             // Search for user in session
-            $pointer = & $_SESSION[ $this->identifier() ];
+            $pointer = &$_SESSION[$this->identifier()];
             if (isset($pointer)) {
-
                 // Load user from session
                 $this->user = unserialize($pointer);
 
@@ -141,54 +144,59 @@ class Core extends CompressableService
             }
         }
 
+        // TODO: Should be removed in next majot version
         // If external init handler is set
         if (is_callable($this->initHandler)) {
             // Call external handler and pass reference on this object
             call_user_func_array($this->initHandler, array(&$this));
         }
-        Event::fire('socal.core.inited', array(&$this));
+
+        // New approach instead of old handler
+        Event::fire(self::EVENT_INIT_END, array(&$this));
+
+        return parent::init($params);
     }
 
     /**
-     * Generic random password generator
-     * @param int $length Password length
+     * Generic random password generator.
      *
+     * @param int $length Password length
      * @return string Generated password
      */
     public function generatePassword($length = 8)
     {
         $password = '';
-        for ($i=0; $i<$length; $i++) {
+        for ($i = 0; $i < $length; $i++) {
             $password .= rand(0, 9);
         }
         return $password;
     }
 
-    /**
-     * @return string Unique module state identifier
-     */
+    /** @return string Unique module state identifier */
     public function identifier()
     {
-        return str_replace(array('\\','/'), '_', __NAMESPACE__.'/'.$this->id.'_auth_'.url()->base());
+        // TODO: Remove url() dependency
+        return str_replace(array('\\', '/'), '_', __NAMESPACE__ . '/' . $this->id . '_auth_' . url()->base());
     }
 
     /**
-     * Finish authorization process and return asynchronous response
-     * @param dbRecord $user Pointer to filled user object
+     * Finish authorization process and return asynchronous response.
      *
+     * @param RecordInterface $user Pointer to filled user object
      * @return array Asynchronous response array
      */
-    public function authorize(dbRecord & $user)
+    public function authorize(RecordInterface & $user)
     {
         // Store pointer to authorized user
-        $this->user = & $user;
+        $this->user = &$user;
 
         $this->authorized = true;
 
+        // TODO: Remove session dependency as storage can be different
         // Save user in session
         $_SESSION[$this->identifier()] = serialize($this->user);
 
-        $this->active = & $this;
+        $this->active = &$this;
 
         $this->update($this->user);
 
@@ -196,22 +204,23 @@ class Core extends CompressableService
         return $this->authorized;
     }
 
-    /** Initiate deauthorization process */
+    /** Initiate de-authorization process */
     public function deauthorize()
     {
         // Tell all ancestors that we are out
         foreach (self::$ancestors as & $ancestor) {
             $ancestor->authorized = false;
             unset($ancestor->user);
+            // TODO: Remove session dependency as storage can be different
             unset($_SESSION[$ancestor->identifier()]);
             setcookie('_cookie_accessToken');
         }
     }
 
-    /** Обработчик сериализации объекта */
+    /** Serialization handler */
     public function __sleep()
     {
         // Remove all unnecessary fields from serialization
-        return array_diff(parent::__sleep(), array( 'authorized', 'user' ));
+        return array_diff(parent::__sleep(), array('authorized', 'user'));
     }
 }
